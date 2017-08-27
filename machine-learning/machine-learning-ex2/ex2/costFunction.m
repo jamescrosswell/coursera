b15d143c99c654853e861188de0a84526b3dd669
James Crosswell <james.crosswell@gmail.com>
2017-08-27 13:59:05 +0200

    Added regularization to the logistic regression cost and gradients

--- machine-learning/machine-learning-ex2/ex2/costFunction.m
+++ machine-learning/machine-learning-ex2/ex2/costFunction.m
@@ -11,7 +11,6 @@ function [J, grad] = costFunction(theta, X, y)
 
   % Compute the cost of a particular choice of theta.
   J = (1 / m) * sum(-y .* log(h) - (1 - y) .* log(1 - h));
-  grad = zeros(size(theta));
 
   % Compute the cost gradients for each parameter in theta
   grad = 1/m * sum((h - y) .* X)'; 
--- machine-learning/machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning/machine-learning-ex2/ex2/costFunctionReg.m
@@ -4,24 +4,31 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %   theta as the parameter for regularized logistic regression and the
 %   gradient of the cost w.r.t. to the parameters. 
 
-% Initialize some useful values
-m = length(y); % number of training examples
+  % Initialize some useful values
+  m = length(y); % number of training examples
+  param_count = length(theta);
+  h = sigmoid(X * theta);
 
-% You need to return the following variables correctly 
-J = 0;
-grad = zeros(size(theta));
+  % Compute the regularized cost of a particular choice of theta.
+  cost_reg = lambda / (2.0 * m) * sum(theta(2:param_count) .^ 2);
+  J = (1 / m) * sum(-y .* log(h) - (1 - y) .* log(1 - h)) + cost_reg;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta.
-%               You should set J to the cost.
-%               Compute the partial derivatives and set grad to the partial
-%               derivatives of the cost w.r.t. each parameter in theta
-
-
-
-
-
-
-% =============================================================
-
+  % Compute the cost gradients for each parameter in theta
+  grad = 1/m * sum((h - y) .* X)'; 
+  
+  % Add regularization factor to the gradients for theta(n) where n > 1
+  grad_reg = lambda / m * theta;
+  grad = [grad(1) ; grad(2:param_count) + grad_reg(2:param_count)];
+  
 end
+
+%!test
+%! data = load('ex2data2.txt');
+%! X = X = mapFeature(data(:,1), data(:,2)); 
+%! y = data(:, 3);
+%! initial_theta = zeros(size(X, 2), 1);
+%! lambda = 1;
+%! [J, grad] = costFunctionReg(initial_theta, X, y, lambda);
+%! assert(round(J * 1000) / 1000 == 0.693);
+%!
+%! assert(size(grad) == size(initial_theta));
