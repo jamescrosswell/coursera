2900f185e8b1c6938c7789b9b9bde22d330d0942
James Crosswell <james.crosswell@gmail.com>
2017-10-02 22:22:15 +0200

    Implemented collaborative filtering for movie recommendations

--- machine-learning/machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -6,57 +6,37 @@ function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, ...
 %   collaborative filtering problem.
 %
 
-% Unfold the U and W matrices from params
-X = reshape(params(1:num_movies*num_features), num_movies, num_features);
-Theta = reshape(params(num_movies*num_features+1:end), ...
-                num_users, num_features);
+  % Unfold the U and W matrices from params
+  X = reshape(params(1:num_movies*num_features), num_movies, num_features);
+  Theta = reshape(params(num_movies*num_features+1:end), num_users, num_features);
+              
+  % Compute the cost function and gradient for collaborative filtering.
+  % Note that R and Y are num_movies × num_users matrices. R(j,i) indicates
+  % whether user j has submitted a rating for movie i. If so, the user's actual
+  % rating is indicated by Y(i,j)
+  h = X * Theta';
+  diff = h - Y;
+  reg_Theta = lambda * sum(Theta(:) .^ 2) / 2; % Regularize user preference weights
+  reg_X = lambda * sum(X(:) .^ 2) / 2; % Regularize the feature weights
+  J = sum((diff.^2)(R==1)) / 2 + reg_Theta + reg_X;
 
-            
-% You need to return the following values correctly
-J = 0;
-X_grad = zeros(size(X));
-Theta_grad = zeros(size(Theta));
+  % Calculate the gradient 
+  X_grad = diff .* R * Theta + lambda * X;
+  Theta_grad = (diff .* R)' * X + lambda * Theta;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost function and gradient for collaborative
-%               filtering. Concretely, you should first implement the cost
-%               function (without regularization) and make sure it is
-%               matches our costs. After that, you should implement the 
-%               gradient and use the checkCostFunction routine to check
-%               that the gradient is correct. Finally, you should implement
-%               regularization.
-%
-% Notes: X - num_movies  x num_features matrix of movie features
-%        Theta - num_users  x num_features matrix of user features
-%        Y - num_movies x num_users matrix of user ratings of movies
-%        R - num_movies x num_users matrix, where R(i, j) = 1 if the 
-%            i-th movie was rated by the j-th user
-%
-% You should set the following variables correctly:
-%
-%        X_grad - num_movies x num_features matrix, containing the 
-%                 partial derivatives w.r.t. to each element of X
-%        Theta_grad - num_users x num_features matrix, containing the 
-%                     partial derivatives w.r.t. to each element of Theta
-%
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% =============================================================
-
-grad = [X_grad(:); Theta_grad(:)];
-
+  grad = [X_grad(:); Theta_grad(:)];
 end
+
+%!test
+%! load ('ex8_movies.mat');
+%! load ('ex8_movieParams.mat');
+%! num_users = 4; num_movies = 5; num_features = 3;
+%! X = X(1:num_movies, 1:num_features);
+%! Theta = Theta(1:num_users, 1:num_features);
+%! Y = Y(1:num_movies, 1:num_users);
+%! R = R(1:num_movies, 1:num_users);
+%! J = cofiCostFunc([X(:) ; Theta(:)], Y, R, num_users, num_movies, num_features, 0);
+%! assert(J, 22.22, 1e-2);          
+%!
+%! J = cofiCostFunc([X(:) ; Theta(:)], Y, R, num_users, num_movies, num_features, 1.5);     
+%! assert(J, 31.34, 1e-2);          
